f555a502ce7d35d29aa52435cfac076a236066bd
TheNotoriousMac <damani.mccaskie@mycavehill.uwi.edu>
2022-09-18 09:04:49 -0400

    Finished styling post side of application.

--- src/components/postSide/PostSide.jsx
+++ src/components/postSide/PostSide.jsx
@@ -1,10 +1,12 @@
 import PostShare from "../postShare/PostShare";
+import Posts from "../posts/Posts";
 import "./PostSide.styles.css";
 
 const PostSide = () => {
   return (
     <div className="PostSide">
         <PostShare />
+        <Posts />
     </div>
   );
 };
---
+++ src/components/posts/Posts.jsx
@@ -0,0 +1,15 @@
+import "./Posts.styles.css";
+import Post from "../post/Post";
+import { PostsData } from "../../Data/PostsData";
+
+const Posts = () => {
+  return (
+    <div className="Posts">
+        {PostsData.map((post, id) => {
+            return <Post data={post} id={id} />
+        })}
+    </div>
+  );
+};
+
+export default Posts;
